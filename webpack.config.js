d9b55d26d4226f896c3a153b5538191c15a8aca5
Tsotne Gvadzabia <tsotne24@hotmail.com>
2021-09-04 21:25:14 +0100

    fixed webpack build bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry:{
         vendor: ["./src/resources/index.scss", "./src/resources/partials/footer.scss", "./src/resources/partials/nav.scss"],
     },
     output:{
-        path: path.resolve(__dirname, './public'),
-        filename: '[name].css'
+        path: path.resolve(__dirname, 'public'),    //Output Destination
     },
+    plugins: [new MiniCssExtractPlugin({
+        filename: '[name].css'
+    })],
     module:{
         rules:[
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
+                    'style-loader', 
+                    MiniCssExtractPlugin.loader,    //Extracts CSS from JS
                     'css-loader',
                     'sass-loader',
                 ],
