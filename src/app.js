d28fdd5a4ce52b542ceb6793d7247f550bc3ad25
Tsotne Gvadzabia <tsotne24@hotmail.com>
2021-10-07 14:48:39 +0100

    switch to gulp

--- src/app.js
+++ src/app.js
@@ -2,23 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const indexRouter = require('./routes/index');
 const path = require('path');
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
 
 const hostname = "localhost";
 const port = 3000;
 const app = express();
-
-if (process.env.NODE_ENV == "Development"){
-    const liveReloadServer = livereload.createServer();
-    liveReloadServer.watch(__dirname + "public");
-    liveReloadServer.server.once("connection", () => {
-        setTimeout(() => {
-            liveReloadServer.refresh("/");
-        }, 50);
-    });
-    app.use(connectLiveReload());
-}
 
 
 // view engine setup
